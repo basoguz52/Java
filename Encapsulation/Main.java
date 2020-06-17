0783b9ccee7ec0fa010ee91b36ba3ab25348d034
Oğuzhan <56599159+basoguz52@users.noreply.github.com>
2020-06-17 14:00:12 +0300

    Create Main.java

---
+++ Encapsulation/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+	/*	Abone abone = new Abone();
+	
+		abone.isim = "Mustafa Murat";
+		abone.bakiye = 200;
+		abone.sehir = "Ordu";
+		
+		abone.gaz_kullan(200);
+    */
+    
+    GelismisAbone abone = new GelismisAbone("Oğuzhan",200,"Ordu");
+    
+    abone.bakiye_ogren();
+    
+	}
+	
+	
+}
