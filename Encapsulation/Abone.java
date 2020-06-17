52f5fc619ce6ff894ef3febe056e4e752c90ac40
Oğuzhan <56599159+basoguz52@users.noreply.github.com>
2020-06-17 14:01:28 +0300

    ssssssssssssssssssssssssssssssssssssssssssssss

---
+++ Encapsulation/Abone.java
@@ -0,0 +1,37 @@
+public class Abone {
+    
+    public String isim;
+    
+    public int bakiye;
+    
+    public String sehir;
+    
+    public void gaz_kullan(int miktar) {
+        
+        if((this.bakiye - miktar) < 0) {
+            
+            System.out.println("Yeterli bakiye yok...");
+            
+        }
+        else {
+            this.bakiye -= miktar;
+            
+            if (this.bakiye <= 0) {
+                
+                System.out.println("Bakiyeniz bitmiştir lütfen en yakın abone merkezine giderek kredi yükleyiniz...");
+                System.out.println("Kredi Limiti 120 TL...");
+            }
+            
+            else {
+                
+                System.out.println("Yeni bakiye" + bakiye);
+            }
+        }
+        
+    }
+    
+    public void bakiye_ogren() {
+        System.out.println("Bakiyeniz: " + bakiye);
+    }
+    
+}
