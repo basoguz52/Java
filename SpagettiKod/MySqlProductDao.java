f4a6c2402850d9af38ebf9daf35ae260bc9c408e
Oğuzhan <56599159+basoguz52@users.noreply.github.com>
2020-06-26 13:56:43 +0300

    Create MySqlProductDao.java

---
+++ SpagettiKod/MySqlProductDao.java
@@ -0,0 +1,14 @@
+public class 
+{
+    private String connectionString;
+    
+    public MySqlProductDao(String connectionString)   
+    {
+        this.connectionString = connectionString;
+    }
+    
+    public void add(Product product)  
+    {
+        System.out.println("MySql Database " + product.getName() + " eklendi...");
+    }
+}
